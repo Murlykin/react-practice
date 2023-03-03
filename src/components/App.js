4c07414c4b481c50e25e454d22c8cf4ffd029d0e
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-03-03 12:56:05 +0200

    9

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,21 @@ import { Layout } from './Layout/Layout';
 import { Component } from 'react';
 import { RecipeList } from './RecipeList/RecipeList';
 import initialRcipes from '../recipes.json';
+import {RecipeForm} from './RecipeForm/RecipeForm';
 
 export class App extends Component {
   state = {
   recipes: initialRcipes,
-}
+  }
+  
+  addRecipe = newRecipe => {
+    this.setState(prevState => {
+      return {
+        recipes: [...prevState.recipes, newRecipe],
+      };
+    });
+  };
+  
   deleteRecipe = recipeId => {
     this.setState(prevState => {
       return {
@@ -18,6 +28,7 @@ export class App extends Component {
   render() {
     return (
       <Layout>
+        <RecipeForm onSave={this.addRecipe} />
         <RecipeList items={this.state.recipes} onDelete={this.deleteRecipe} />
         <GlobalStyle />
       </Layout>
